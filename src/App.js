6f3b67946e2c496a6ca37140f484765787041b38
ogunnaike <fivestonestech@gmail.com>
2024-02-01 21:33:35 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Matthew <code>src/App.js</code> I am checking if this host will work out
         </p>
         <a
           className="App-link"
